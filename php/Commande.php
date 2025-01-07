bc92447ab60690c9ccd022f9768282e94c23d538
swanbreton <swan.breton75@gmail.com>
2025-01-07 11:47:34 +0100

    ajout code BDD

--- php/Commande.php
+++ php/Commande.php
@@ -1,4 +1,9 @@
 <?php
+// Activer l'affichage des erreurs pour le débogage
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 try {
     // Connexion à la base de données
     $pdo = new PDO('mysql:host=localhost;dbname=blindtaste', 'root', '');
